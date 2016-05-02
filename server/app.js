244ea130fbeb81c6064dfd390ae1e3e4c54b29e2
Ransom <ranmantwo@gmail.com>
2016-05-02 09:56:25 -0400

    Better debounce?!!!!

--- server/app.js
+++ server/app.js
@@ -63,13 +63,6 @@ function debounce(func, wait, immediate) {
   var timeout;
   return function () {
     var context = this, args = arguments;
-    var later = function () {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    };
-    var callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
+    func.apply(context, args);
   };
 }
